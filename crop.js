0e7cf16c3b2d9d10774416f5d880d567aca62306
Emily A. Bellows <emily.a.bellows@gmail.com>
2015-07-19 15:05:01 -0700

    ~

--- crop.js
+++ crop.js
@@ -50,7 +50,7 @@ function center_of_edginess(img_data, y_param) {
     var sum_x = 0;
     var sum_y = 0;
     var sum_edge = 0;
-    for (var y = 0; y < height - 1; y++) {
+    for (var y = 0; y < height; y++) {
         for (var x = 0; x < width - 1; x++) {
             var here = (x + y * width) * 4;
             var r1 = data[here + 0];
@@ -64,8 +64,8 @@ function center_of_edginess(img_data, y_param) {
             var g_squared = (g1 - g2) * (g1 - g2);
             var b_squared = (b1 - b2) * (b1 - b2);
             var dist = Math.sqrt(r_squared + g_squared + b_squared);
-            sum_x += x * dist;
-            sum_y += y * dist;
+            sum_x += (x + 1) * dist;
+            sum_y += (y + 1) * dist;
             sum_edge += dist;
         }
     }
@@ -102,62 +102,6 @@ function draw_center_of_edginess(img) {
     return result;
 }
 var MAX_DISTANCE = 441;
-var ColorDistHistogram = (function () {
-    function ColorDistHistogram() {
-        this.arr = new Uint32Array(MAX_DISTANCE);
-    }
-    ColorDistHistogram.prototype.reset = function () {
-        var arr = this.arr;
-        for (var i = 0; i < MAX_DISTANCE; i++) {
-            arr[i] = 0;
-        }
-    };
-    ColorDistHistogram.prototype.calculate = function (img_data, x, y, width, height) {
-        var data = img_data.data;
-        var arr = this.arr;
-        var index = (function () {
-            var row_length = img_data.width;
-            return function (i, j) {
-                return (i + j * row_length) * 4;
-            };
-        })();
-        var last_x = x + width - 1;
-        var last_y = y + height - 1;
-        for (; y < last_y; y++) {
-            for (; x < last_x; x++) {
-                var here = index(x, y);
-                var r1 = data[here + 0];
-                var g1 = data[here + 1];
-                var b1 = data[here + 2];
-                var here = index(x + 1, y);
-                var r2 = data[here + 0];
-                var g2 = data[here + 1];
-                var b2 = data[here + 2];
-                var r_squared = (r1 - r2) * (r1 - r2);
-                var g_squared = (g1 - g2) * (g1 - g2);
-                var b_squared = (b1 - b2) * (b1 - b2);
-                var dist = Math.sqrt(r_squared + g_squared + b_squared) | 0;
-                arr[dist] += 1;
-            }
-        }
-    };
-    ColorDistHistogram.prototype.total_entropy = function () {
-        var total = 0;
-        var arr = this.arr;
-        for (var i = 0; i < MAX_DISTANCE; i++) {
-            total += arr[i];
-        }
-        var sum = 0;
-        for (var i = 0; i < MAX_DISTANCE; i++) {
-            var p = (i * arr[i]) / total;
-            if (p == 0)
-                continue;
-            sum += p * (Math.log(p) / Math.LOG2E);
-        }
-        return -sum;
-    };
-    return ColorDistHistogram;
-})();
 function draw_entropy_map(img_data, out) {
     var data = img_data.data;
     var index = (function () {
@@ -205,96 +149,6 @@ function make_entropy_canvas(img) {
     ctx.putImageData(out, 0, 0, 0, 0, canvas.width, canvas.height);
     return canvas;
 }
-var CroppingControl = (function () {
-    function CroppingControl(img_data) {
-        this.img_data = img_data;
-        this.histo = new ColorDistHistogram();
-        this.x = 0;
-        this.y = 0;
-        this.width = img_data.width;
-        this.height = img_data.height;
-    }
-    CroppingControl.prototype.entropy = function (slice_amount, x, y, width, height) {
-        this.histo.calculate(this.img_data, x, y, width, height);
-        var result = this.histo.total_entropy();
-        this.histo.reset();
-        return result;
-    };
-    CroppingControl.prototype.entropy_top = function (slice_amount) {
-        return this.entropy(slice_amount, this.x, this.y, this.width, slice_amount);
-    };
-    CroppingControl.prototype.entropy_bottom = function (slice_amount) {
-        return this.entropy(slice_amount, this.x, this.y + this.height - slice_amount, this.width, slice_amount);
-    };
-    CroppingControl.prototype.entropy_left = function (slice_amount) {
-        return this.entropy(slice_amount, this.x, this.y, slice_amount, this.height);
-    };
-    CroppingControl.prototype.entropy_right = function (slice_amount) {
-        return this.entropy(slice_amount, this.x + this.width - slice_amount, this.y, slice_amount, this.height);
-    };
-    CroppingControl.prototype.slice_top = function (slice_amount) {
-        this.y += slice_amount;
-        this.height -= slice_amount;
-    };
-    CroppingControl.prototype.slice_bottom = function (slice_amount) {
-        this.height -= slice_amount;
-    };
-    CroppingControl.prototype.slice_left = function (slice_amount) {
-        this.x += slice_amount;
-        this.width -= slice_amount;
-    };
-    CroppingControl.prototype.slice_right = function (slice_amount) {
-        this.width -= slice_amount;
-    };
-    return CroppingControl;
-})();
-function draw_entropy_cropped(img) {
-    var canvas = document.createElement("canvas");
-    var target = 250;
-    var ratio = Math.max(img.naturalWidth, img.naturalHeight) / target;
-    canvas.width = (img.naturalWidth / ratio) | 0;
-    canvas.height = (img.naturalHeight / ratio) | 0;
-    var ctx = canvas.getContext("2d");
-    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-    var img_data = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    var cropping_ctl = new CroppingControl(img_data);
-    while (cropping_ctl.height * 16 / 9 > cropping_ctl.width) {
-        var slice_amount = 4;
-        var top_entropy = cropping_ctl.entropy_top(slice_amount);
-        var bottom_entropy = cropping_ctl.entropy_bottom(slice_amount);
-        if (top_entropy < bottom_entropy) {
-            cropping_ctl.slice_top(slice_amount);
-        }
-        else {
-            cropping_ctl.slice_bottom(slice_amount);
-        }
-    }
-    while (cropping_ctl.width * 9 / 16 > cropping_ctl.height) {
-        var slice_amount = 4;
-        var left_entropy = cropping_ctl.entropy_left(slice_amount);
-        var right_entropy = cropping_ctl.entropy_right(slice_amount);
-        if (left_entropy < right_entropy) {
-            cropping_ctl.slice_left(slice_amount);
-        }
-        else {
-            cropping_ctl.slice_right(slice_amount);
-        }
-    }
-    var result = document.createElement("canvas");
-    if (img.naturalWidth * 9 / 16 > img.naturalHeight) {
-        result.width = 272;
-        result.height = (result.width * 9 / 16) | 0;
-    }
-    else {
-        result.height = 152;
-        result.width = (result.height * 16 / 9) | 0;
-    }
-    var resultCtx = result.getContext("2d");
-    resultCtx.drawImage(img, (cropping_ctl.x * ratio) | 0, (cropping_ctl.y * ratio) | 0, (cropping_ctl.width * ratio) | 0, (cropping_ctl.height * ratio) | 0, 0, 0, result.width, result.height);
-    result.style.maxWidth = 272 + 'px';
-    result.style.height = 'auto';
-    return result;
-}
 draw_all_images('./sample1.jpg');
 draw_all_images('./sample2.jpg');
 draw_all_images('./sample3.jpg');
@@ -303,6 +157,8 @@ draw_all_images('./sample5.jpg');
 draw_all_images('./sample6.jpg');
 draw_all_images('./sample7.jpg');
 draw_all_images('./sample8.png');
+draw_all_images('./sample9.jpg');
+draw_all_images('./sample10.jpg');
 draw_all_images('./huge_sample1.jpg');
 draw_all_images('./huge_sample2.jpg');
 draw_all_images('./huge_sample3.jpg');
--- crop.ts
+++ crop.ts
@@ -67,7 +67,7 @@ function center_of_edginess(img_data: ImageData, y_param: number): CenterOfEdgin
   var sum_y = 0;
   var sum_edge = 0;
 
-  for (var y = 0; y < height - 1; y++) {
+  for (var y = 0; y < height; y++) {
     for (var x = 0; x < width - 1; x++) {
       var here = (x + y * width) * 4;
       var r1 = data[here + 0];
@@ -150,75 +150,6 @@ function draw_center_of_edginess(img: HTMLImageElement): HTMLCanvasElement {
 
 const MAX_DISTANCE: number = 441;
 
-class ColorDistHistogram {
-  constructor() {
-    this.arr = new Uint32Array(MAX_DISTANCE);
-  }
-
-  reset() {
-    var arr = this.arr;
-    for (var i = 0; i < MAX_DISTANCE; i++) {
-      arr[i] = 0;
-    }
-  }
-
-  calculate(img_data: ImageData, x: number, y: number, width: number, height: number): void {
-    var data = img_data.data;
-    var arr = this.arr;
-
-    var index = (function() {
-      var row_length = img_data.width;
-
-      return function(i: number, j: number): number {
-        return (i + j * row_length) * 4;
-      }
-    })();
-
-    var last_x = x + width - 1;
-    var last_y = y + height - 1;
-
-    for (; y < last_y; y++) {
-      for (; x < last_x; x++) {
-        var here = index(x, y);
-        var r1 = data[here + 0];
-        var g1 = data[here + 1];
-        var b1 = data[here + 2];
-
-        var here = index(x + 1, y);
-        var r2 = data[here + 0];
-        var g2 = data[here + 1];
-        var b2 = data[here + 2];
-
-        var r_squared = (r1 - r2) * (r1 - r2);
-        var g_squared = (g1 - g2) * (g1 - g2);
-        var b_squared = (b1 - b2) * (b1 - b2);
-        var dist = Math.sqrt(r_squared + g_squared + b_squared) | 0;
-
-        arr[dist] += 1;
-      }
-    }
-  }
-
-  total_entropy(): number {
-    var total = 0;
-    var arr = this.arr;
-    for (var i = 0; i < MAX_DISTANCE; i++) {
-      total += arr[i];
-    }
-
-    var sum = 0;
-    for (var i = 0; i < MAX_DISTANCE; i++) {
-      var p = (i * arr[i]) / total;
-      if (p == 0) continue;
-      sum += p * (Math.log(p) / Math.LOG2E);
-    }
-
-    return -sum;
-  }
-
-  public arr: Uint32Array;
-}
-
 function draw_entropy_map(img_data: ImageData, out: ImageData) {
   var data = img_data.data;
 
@@ -277,148 +208,6 @@ function make_entropy_canvas(img: HTMLImageElement): HTMLCanvasElement {
   return canvas;
 }
 
-class CroppingControl {
-  constructor(img_data: ImageData) {
-    this.img_data = img_data;
-    this.histo = new ColorDistHistogram();
-    this.x = 0;
-    this.y = 0;
-    this.width = img_data.width;
-    this.height = img_data.height;
-  }
-
-  public img_data: ImageData;
-  private histo: ColorDistHistogram;
-  public x: number;
-  public y: number;
-  public width: number;
-  public height: number;
-
-  entropy(slice_amount: number, x: number, y: number, width: number, height: number): number {
-    this.histo.calculate(this.img_data, x, y, width, height);
-    var result = this.histo.total_entropy();
-    this.histo.reset();
-    return result;
-  }
-
-  entropy_top(slice_amount: number): number {
-    return this.entropy(
-      slice_amount,
-      this.x,
-      this.y,
-      this.width,
-      slice_amount);
-  }
-
-  entropy_bottom(slice_amount: number): number {
-    return this.entropy(
-      slice_amount,
-      this.x,
-      this.y + this.height - slice_amount,
-      this.width,
-      slice_amount);
-  }
-
-  entropy_left(slice_amount: number): number {
-    return this.entropy(
-      slice_amount,
-      this.x,
-      this.y,
-      slice_amount,
-      this.height);
-  }
-
-  entropy_right(slice_amount: number): number {
-    return this.entropy(
-      slice_amount,
-      this.x + this.width - slice_amount,
-      this.y,
-      slice_amount,
-      this.height);
-  }
-
-  slice_top(slice_amount: number): void {
-    this.y += slice_amount;
-    this.height -= slice_amount;
-  }
-
-  slice_bottom(slice_amount: number): void {
-    this.height -= slice_amount;
-  }
-
-  slice_left(slice_amount: number): void {
-    this.x += slice_amount;
-    this.width -= slice_amount;
-  }
-
-  slice_right(slice_amount: number): void {
-    this.width -= slice_amount;
-  }
-}
-
-function draw_entropy_cropped(img: HTMLImageElement): HTMLCanvasElement {
-  var canvas: HTMLCanvasElement = document.createElement("canvas");
-
-  var target = 250;
-  var ratio = Math.max(img.naturalWidth, img.naturalHeight) / target;
-  canvas.width = (img.naturalWidth / ratio) | 0;
-  canvas.height = (img.naturalHeight / ratio) | 0;
-
-  var ctx: CanvasRenderingContext2D = canvas.getContext("2d");
-  ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-  var img_data: ImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-
-  var cropping_ctl = new CroppingControl(img_data);
-
-  while (cropping_ctl.height * 16 / 9 > cropping_ctl.width) {
-    var slice_amount = 4
-
-    var top_entropy = cropping_ctl.entropy_top(slice_amount);
-    var bottom_entropy = cropping_ctl.entropy_bottom(slice_amount);
-
-    if (top_entropy < bottom_entropy) {
-      cropping_ctl.slice_top(slice_amount);
-    } else {
-      cropping_ctl.slice_bottom(slice_amount);
-    }
-  }
-
-  while (cropping_ctl.width * 9 / 16 > cropping_ctl.height) {
-    var slice_amount = 4
-
-    var left_entropy = cropping_ctl.entropy_left(slice_amount);
-    var right_entropy = cropping_ctl.entropy_right(slice_amount);
-
-    if (left_entropy < right_entropy) {
-      cropping_ctl.slice_left(slice_amount);
-    } else {
-      cropping_ctl.slice_right(slice_amount);
-    }
-  }
-
-  var result: HTMLCanvasElement = document.createElement("canvas");
-  if (img.naturalWidth * 9 / 16 > img.naturalHeight) {
-    result.width = 272;
-    result.height = (result.width * 9 / 16) | 0;
-  } else {
-    result.height = 152;
-    result.width = (result.height * 16 / 9) | 0;
-  }
-
-  var resultCtx: CanvasRenderingContext2D = result.getContext("2d");
-  resultCtx.drawImage(
-    img,
-    (cropping_ctl.x * ratio) | 0,
-    (cropping_ctl.y * ratio) | 0,
-    (cropping_ctl.width * ratio) | 0,
-    (cropping_ctl.height * ratio) | 0,
-    0, 0, result.width, result.height);
-
-  result.style.maxWidth = 272 + 'px';
-  result.style.height = 'auto';
-  return result;
-}
-
 draw_all_images('./sample1.jpg');
 draw_all_images('./sample2.jpg');
 draw_all_images('./sample3.jpg');
@@ -427,6 +216,8 @@ draw_all_images('./sample5.jpg');
 draw_all_images('./sample6.jpg');
 draw_all_images('./sample7.jpg');
 draw_all_images('./sample8.png');
+draw_all_images('./sample9.jpg');
+draw_all_images('./sample10.jpg');
 draw_all_images('./huge_sample1.jpg');
 draw_all_images('./huge_sample2.jpg');
 draw_all_images('./huge_sample3.jpg');
